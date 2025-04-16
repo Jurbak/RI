208fdc5fa4c4435264663e05b111f89505d99da3
Jurbak <jujurberbakti52@gmail.com>
2025-04-16 16:47:54 +0700

    second hehe

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
 const CACHE_NAME = "scrap-cache-v1";
+
+const chapterUrls = Array.from({ length: 2334 }, (_, i) => `chapter/${i + 1}.html`);
+
 const urlsToCache = [
   "index.html",
   "style.css",
-  "output.html"
+  "output.html",
+  "offline.html",
+  ...chapterUrls
 ];
 
 self.addEventListener("install", event => {
@@ -16,5 +21,11 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(() => {
+        // fallback untuk halaman HTML
+        if (event.request.destination === 'document') {
+          return caches.match("offline.html");
+        }
+      })
   );
 });
